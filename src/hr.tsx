d047fdd2852394ca2c8784d5ed5f70254e49ca11
szkj-web-he <xuejie.he@datareachable.com>
2022-09-20 09:07:20 +0800

    1

---
+++ src/hr.tsx
@@ -0,0 +1,35 @@
+/**
+ * @file hr
+ * @date 2022-09-20
+ * @author xuejie.he
+ * @lastModify xuejie.he 2022-09-20
+ */
+/* <------------------------------------ **** DEPENDENCE IMPORT START **** ------------------------------------ */
+/** This section will include all the necessary dependence for this tsx file */
+import React, { useState } from "react";
+import from "./"
+/* <------------------------------------ **** DEPENDENCE IMPORT END **** ------------------------------------ */
+/* <------------------------------------ **** INTERFACE START **** ------------------------------------ */
+/** This section will include all the interface for this tsx file */
+interface TempProps {}
+/* <------------------------------------ **** INTERFACE END **** ------------------------------------ */
+/* <------------------------------------ **** FUNCTION COMPONENT START **** ------------------------------------ */
+const Temp: React.FC<TempProps> = () => {
+    /* <------------------------------------ **** STATE START **** ------------------------------------ */
+    /************* This section will include this component HOOK function *************/
+    /* <------------------------------------ **** STATE END **** ------------------------------------ */
+    /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
+    /************* This section will include this component parameter *************/
+    /* <------------------------------------ **** PARAMETER END **** ------------------------------------ */
+    /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
+    /************* This section will include this component general function *************/
+    /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
+    return <div className="hr">
+        <div className="hr_left"></div>
+
+        <div className="hr_right"></div>
+
+    </div>;
+};
+/* <------------------------------------ **** FUNCTION COMPONENT END **** ------------------------------------ */
+export default Temp;
