4716f69c64fabd9bfeb97b4595de98f7d1f31fc3
szkj-web-he <xuejie.he@datareachable.com>
2022-12-05 17:14:33 +0800

    2D多选完成

--- src/item.tsx
+++ src/item.tsx
@@ -7,8 +7,6 @@
 /* <------------------------------------ **** DEPENDENCE IMPORT START **** ------------------------------------ */
 /** This section will include all the necessary dependence for this tsx file */
 import React from "react";
-import { isMobile } from "./isMobile";
-import { useRef } from "react";
 /* <------------------------------------ **** DEPENDENCE IMPORT END **** ------------------------------------ */
 /* <------------------------------------ **** INTERFACE START **** ------------------------------------ */
 /** This section will include all the interface for this tsx file */
@@ -24,9 +22,7 @@ interface TempProps extends React.HTMLAttributes<HTMLDivElement> {
 const Temp: React.FC<TempProps> = ({ active, onClick, children, ...props }) => {
     /* <------------------------------------ **** STATE START **** ------------------------------------ */
     /************* This section will include this component HOOK function *************/
-    const touchStart = useRef(false);
 
-    const touchMove = useRef(false);
     /* <------------------------------------ **** STATE END **** ------------------------------------ */
     /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
     /************* This section will include this component parameter *************/
@@ -35,51 +31,12 @@ const Temp: React.FC<TempProps> = ({ active, onClick, children, ...props }) => {
     /************* This section will include this component general function *************/
 
     const handleClick = () => {
-        const mobileStatus = isMobile();
-        if (mobileStatus) {
-            return;
-        }
-        onClick();
-    };
-
-    const handleTouchStart = () => {
-        const mobileStatus = isMobile();
-        if (!mobileStatus) {
-            return;
-        }
-        touchStart.current = true;
-        touchMove.current = false;
-    };
-
-    const handleTouchMove = () => {
-        const mobileStatus = isMobile();
-        if (!mobileStatus) {
-            return;
-        }
-        touchMove.current = true;
-    };
-
-    const handleTouchEnd = () => {
-        if (touchMove.current) {
-            return;
-        }
-
-        if (!touchStart.current) {
-            return;
-        }
         onClick();
     };
 
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
     return (
-        <div
-            className={`item${active ? " active" : ""}`}
-            onClick={handleClick}
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
-            {...props}
-        >
+        <div className={`item${active ? " active" : ""}`} onClick={handleClick} {...props}>
             {children}
         </div>
     );
--- src/unit.ts
+++ src/unit.ts
@@ -26,3 +26,21 @@ export const getScrollBody = (node: HTMLElement | null): HTMLElement | undefined
     }
     return el;
 };
+
+export const getScrollValue = (): {
+    x: number;
+    y: number;
+} => {
+    let x = window.scrollX || window.pageXOffset;
+    let y = window.scrollY || window.pageYOffset;
+    const node = document.documentElement || document.body.parentNode;
+    if (!x) {
+        x = (typeof node.scrollLeft === "number" ? node : document.body).scrollLeft;
+    } else if (!y) {
+        y = (typeof node.scrollTop === "number" ? node : document.body).scrollTop;
+    }
+    return {
+        x,
+        y,
+    };
+};
