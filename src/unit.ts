32496f19bc2608f57ae86883d8d67b6ba398ebfa
szkj-web-he <83932083+szkj-web-he@users.noreply.github.com>
2022-12-12 14:14:21 +0800

    1

--- src/unit.ts
+++ src/unit.ts
@@ -2,3 +2,32 @@ export interface OptionProps {
     code: string;
     content: string;
 }
+
+/**
+ * 深克隆
+ */
+export const deepCloneData = <T>(data: T): T => {
+    if (data == null) {
+        return data;
+    }
+
+    return JSON.parse(JSON.stringify(data)) as T;
+};
+
+export const getScrollValue = (): {
+    x: number;
+    y: number;
+} => {
+    let x = window.scrollX || window.pageXOffset;
+    let y = window.scrollY || window.pageYOffset;
+    const node = document.documentElement || document.body.parentNode;
+    if (!x) {
+        x = (typeof node.scrollLeft === "number" ? node : document.body).scrollLeft;
+    } else if (!y) {
+        y = (typeof node.scrollTop === "number" ? node : document.body).scrollTop;
+    }
+    return {
+        x,
+        y,
+    };
+};
