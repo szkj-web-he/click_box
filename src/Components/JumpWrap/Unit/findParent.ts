f8d22ef90b9aa9529568076e53b33d1456b3f616
szkj-web-he <xuejie.he@datareachable.com>
2023-04-07 16:51:32 +0800

    完成答案回溯

--- src/Components/JumpWrap/Unit/findParent.ts
+++ src/Components/JumpWrap/Unit/findParent.ts
@@ -3,17 +3,7 @@
  * 直到当前这个元素是要找的那位时
  */
 export const findParent = (el: HTMLElement, parent: HTMLElement): number => {
-    const offsetParent = el.offsetParent;
-    let top = el.offsetTop;
-    if (offsetParent === parent.parentElement) {
-        return top;
-    }
-
-    let p = el.parentElement;
-
-    while (p !== parent) {
-        top += p?.offsetTop ?? 0;
-        p = p?.parentElement ?? null;
-    }
-    return top;
+    const parentRect = parent.getBoundingClientRect();
+    const elRect = el.getBoundingClientRect();
+    return parent.scrollTop + (elRect.top - parentRect.top);
 };
