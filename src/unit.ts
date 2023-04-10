0842e020e1aaf8744dd4f8eeb13c4ca48d6ad657
szkj-web-he <xuejie.he@datareachable.com>
2023-04-10 11:24:31 +0800

    完成答案回溯

--- src/unit.ts
+++ src/unit.ts
@@ -1,10 +1,9 @@
+import { comms } from ".";
+
 export interface OptionProps {
     code: string;
     content: string;
 }
-
-
-
 
 export const getScrollValue = (): {
     x: number;
@@ -22,4 +21,26 @@ export const getScrollValue = (): {
         x,
         y,
     };
+};
+
+export const deepCloneData = <T>(res: T): T => {
+    if (typeof res === "object") {
+        return JSON.parse(JSON.stringify(res)) as T;
+    }
+
+    return res;
+};
+
+export const getState = (): OptionProps[] => {
+    const state = comms.state as Record<string, "1" | "0">;
+    const options = comms.config.options ?? [];
+    const arr: Array<OptionProps> = [];
+    for (const key in state) {
+        const keyVal = key.split("#")[1];
+        if (state[key] === "1") {
+            const data = options.find((item) => item.code === keyVal);
+            data && arr.push(deepCloneData(data));
+        }
+    }
+    return arr;
 };
