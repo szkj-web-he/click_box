a01dff07033ffa551156e70361d3e9ec2e5ab09c
szkj-web-he <xuejie.he@datareachable.com>
2023-04-10 11:45:52 +0800

    完成答案回溯

--- src/unit.ts
+++ src/unit.ts
@@ -1,4 +1,27 @@
+import { comms } from ".";
+
 export interface OptionProps {
     code: string;
     content: string;
 }
+
+const deepCloneData = <T>(res: T): T => {
+    if (typeof res === "object") {
+        return JSON.parse(JSON.stringify(res)) as T;
+    }
+    return res;
+};
+
+export const getState = (): OptionProps[] => {
+    const state = comms.state as Record<string, "1" | "0">;
+    const options = comms.config.options ?? [];
+    const arr: Array<OptionProps> = [];
+    for (const key in state) {
+        const keyVal = key.split("#")[1];
+        if (state[key] === "1") {
+            const data = options.find((item) => item.code === keyVal);
+            data && arr.push(deepCloneData(data));
+        }
+    }
+    return arr;
+};
